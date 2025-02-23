4413a8ff318b62ad6dfbdd4d82668eaca4eb58d3
JC Botelho <joaoclaudiobr92@gmail.com>
2025-02-23 20:35:34 -0300

    ajustes das dependencias do setup da lib

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name="lib_kerberos_auth",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "pymysql",
+        "boto3"
+    ],
     python_requires=">=3.6",
 )
